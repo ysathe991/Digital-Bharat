40bf951e9d82e682f81badc697de70abc783e662
Yogesh Sathe <ysathe991@gmail.com>
2024-02-23 12:35:48 +0530

    project structure defined

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../components/layout/Layout.jsx'
+
+function home() {
+  return (
+    <div>
+      <Layout>home</Layout>
+    </div>
+  )
+}
+
+export default home
